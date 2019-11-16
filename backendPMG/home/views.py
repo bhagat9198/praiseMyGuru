133c9c57d7d710690ba3689a32f6fe970433984a
bhagat Singh <dark@localhost.localdomain>
2019-11-16 20:12:26 +0530

    added media(not working)

--- backendPMG/home/views.py
+++ backendPMG/home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home(request):
-  return render(request, 'home/home.html')
+  if request.method == 'post':
+    uplaodedFile = request.FILES('doc')
+    print(uplaodedFile.name)
+    print(uplaodedFile.size)
+    fs = FileSystemStorage()
+    fs.save(uplaodedFile.name, uplaodedFile)
+  else:
+    return render(request, 'home/home.html')
